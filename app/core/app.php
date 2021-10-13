21a6f932025e90b1b816fcf5f58123f21845a9f4
mangyobagus <riokomang03@gmail.com>
2021-10-13 12:50:08 +0800

    htaccess

--- app/core/app.php
+++ app/core/app.php
@@ -2,4 +2,26 @@
 public function __construct() {
     echo "Web MVC Siap Dijalankan!";
   }
+
+
+  public function __construct() {
+    $url = $this->parseURL();
+    var_dump($url);
+    }
+    public function parseURL() {
+    if(isset($_GET[‘url’])) {
+    $url = $_GET[‘url’];return $url;
+    }
+    }
+
+    public function parseURL() {
+      if(isset($_GET[‘url’])) {
+      $url = rtrim($_GET[‘url’],”/”);
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode(‘/’, $url);
+      return $url;
+      }
+      }
 ?>
+
+
